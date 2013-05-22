ec8a492f3d8e1b842fb9fecac39edffe14e8f13b
pompopo <pompopo@gmail.com>
2013-05-22 23:50:55 +0900

    initial commit

---
+++ PMTwitter.h
@@ -0,0 +1,13 @@
+//
+//  PMTwitter.h
+//  PMTwitter
+//
+//  Created by pom on 13/05/22.
+//  Copyright (c) 2013年 com.gmail.pompopo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMTwitter : NSObject
+
+@end
---
+++ PMTwitter.m
@@ -0,0 +1,13 @@
+//
+//  PMTwitter.m
+//  PMTwitter
+//
+//  Created by pom on 13/05/22.
+//  Copyright (c) 2013年 com.gmail.pompopo. All rights reserved.
+//
+
+#import "PMTwitter.h"
+
+@implementation PMTwitter
+
+@end
